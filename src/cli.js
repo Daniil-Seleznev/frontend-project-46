121668b749df449bd874cf2483be2fcf5b8142fb
Daniil-Seleznev <seleznve.d.ole@gmail.com>
2026-03-26 01:06:42 +0300

    changed cli

--- src/cli.js
+++ src/cli.js
@@ -7,16 +7,16 @@ const program = new Command();
 
 program
   .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference')
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format', 'default')
-  .arguments('<filepath1> <filepath2>')
+  .argument('<filepath1>', 'first file')
+  .argument('<filepath2>', 'second file')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
-    const data1 = parseFile(filepath1);
-    const data2 = parseFile(filepath2);
-
-    const diff = genDiff(data1, data2, options.format);
+    // Передаём строки, а не объект
+    const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
   });
 
-program.parse(process.argv);
+program.parse();
+
